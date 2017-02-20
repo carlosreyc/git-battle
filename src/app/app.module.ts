733ee50233ab5b46ec136a0baf7b688cae0923a9
U-Charlie-PC\Charlie <sharlyebonham@gmail.com>
2017-02-20 12:13:06 -0600

    V1.1 Folders restructured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { PromptComponent } from './prompt/prompt.component';
+import { ResultsComponent } from './results/results.component';
+import { HomeComponent } from './home/home.component';
+import { UserDetail } from './user-detail/user-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
+  imports:      [ 
     BrowserModule,
     FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    HttpModule, 
+    RouterModule.forRoot([
+    {
+      path: 'players',
+      component: PromptComponent
+    },
+    {
+      path: '',
+      component: HomeComponent
+    },
+    {
+      path: 'results/:player1/:player2',
+      component: ResultsComponent
+    }
+  ]) ],
+  declarations: [ AppComponent, PromptComponent, HomeComponent, ResultsComponent, UserDetail, HomeComponent ],
+  bootstrap:    [ AppComponent ],
+  
 })
 export class AppModule { }
